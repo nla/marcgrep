c4182b4066c60a059ef6d52dd3e54c4a7b97da1d
shaun <scoen@nla.gov.au>
2021-05-11 17:04:59 +1000

    Added leader printout to plaintext marc output

--- src/marcgrep/destinations/plaintext.clj
+++ src/marcgrep/destinations/plaintext.clj
@@ -9,6 +9,9 @@
 (def ms-between-flushes 10000)
 
 (defn write-pretty-record [^Record record ^BufferedWriter out included-fields]
+  (let [^Leader lead (.getLeader record)]
+    (do (.write out (.marshal lead))
+        (.write out "\r\n")))
   (doseq [^VariableField f (.getVariableFields record)]
     (let [tag (.getTag f)]
       (when (or (not included-fields)
